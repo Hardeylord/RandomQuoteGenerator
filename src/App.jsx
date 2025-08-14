cb443351d84ed704a037dd503b9680b414d26fd8
Hardeylord <adedipupoadeagbo@gmail.com>
2025-08-14 01:57:33 +0100

    quote error

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
       setisLoading(false);
       console.log(QUOTE.content);
     } catch (error) {
-      alert(error);
+      alert("error fetching quote check network connection");
     }
   };
   return (
